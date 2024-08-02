bfd16cb630a28e0b6c0f4c3461b4b3b25ed71bd0
Abhishek Manhar <abhimanharkhan25@gmail.com>
2024-08-02 21:25:22 +0530

    Update portfolio.config.ts

--- src/config/portfolio.config.ts
+++ src/config/portfolio.config.ts
@@ -4,11 +4,11 @@ export const portfolioConfig: Portfolio = {
   name: "Abhishek Manhar",
   tagline: "I gaslight rocks 🪨 to think 🧠",
   bio: "Hey 👋 Abhishek Manhar here! I am a versatile software and web developer. I specialize in building robust web applications, intuitive mobile applications, reliable backend servers, and innovative embedded devices. With a passion for creating seamless digital experiences, I bring a blend of technical expertise and creative problem-solving to every project.",
-  resume: "https://drive.google.com/file/d/1srIHh6uw_lW5PWJzwOw5JvKPhczZJ3P3/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1QY6GqKoMEwyehW4YrOlw0TCr8hyAmrsM/view?usp=sharing",
   links: {
     github: "https://github.com/abhishekmanhar",
-    twitter: "https://x.com/rds_agi",
-    mail: "mailto:official.abhishekmanhr004@gmail.com",
+    twitter: "https://x.com/abhishekmnhr?t=uRnxwO-L7ZK64FIvjShNRA&s=09&mx=2",
+    mail: "mailto:abhishekmanhr004@gmail.com",
     instagram: "https://www.instagram.com/abhishekmnhr/",
     linkedin: "https://www.linkedin.com/in/abhishek-manhar-developer/",
   }
